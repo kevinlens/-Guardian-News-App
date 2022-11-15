00385e3f40ba2f2ba9534b287585bcdd1f495d30
Kevin <kevinlenske@proton.me>
2022-11-14 20:39:00 -0800

    added header, logo, headline, opinion articles section, and some aside article

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '../src/App/App'
 import './index.scss'
+import './styles/typography.scss'
+import './styles/variables.scss'
 
 // This is for React 18, which introduces a new root API
 const root = ReactDOM.createRoot(document.getElementById('root'));
